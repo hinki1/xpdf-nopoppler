4ff50d795248137b39ecb7ee93e7418109e03a15
Matthias Hinkfoth <hinki@abwesend.de>
2018-02-06 22:10:31 +0100

    updated SecurityHandler

--- xpdf/SecurityHandler.cc
+++ xpdf/SecurityHandler.cc
@@ -33,9 +33,9 @@ SecurityHandler *SecurityHandler::make(PDFDoc *docA, Object *encryptDictA) {
   if (filterObj.isName("Standard")) {
     secHdlr = new StandardSecurityHandler(docA, encryptDictA);
   } else if (filterObj.isName()) {
-      error(errSyntaxError, -1, "Couldn't find the '{0:s}' security handler",
-	    filterObj.getName());
-      secHdlr = NULL;
+    error(errSyntaxError, -1, "Couldn't find the '{0:s}' security handler",
+	  filterObj.getName());
+    secHdlr = NULL;
   } else {
     error(errSyntaxError, -1,
 	  "Missing or invalid 'Filter' entry in encryption dictionary");
@@ -136,137 +136,153 @@ StandardSecurityHandler::StandardSecurityHandler(PDFDoc *docA,
   encryptDictA->dictLookup("UE", &userEncObj);
   encryptDictA->dictLookup("P", &permObj);
   doc->getXRef()->getTrailerDict()->dictLookup("ID", &fileIDObj);
-  if (versionObj.isInt() &&
-      revisionObj.isInt() &&
-      permObj.isInt() &&
-      ownerKeyObj.isString() &&
-      userKeyObj.isString()) {
-    encVersion = versionObj.getInt();
-    encRevision = revisionObj.getInt();
-    if ((encRevision <= 4 &&
-	 ownerKeyObj.getString()->getLength() == 32 &&
-	 userKeyObj.getString()->getLength() == 32) ||
-	((encRevision == 5 || encRevision == 6) &&
-	 // the spec says 48 bytes, but Acrobat pads them out longer
-	 ownerKeyObj.getString()->getLength() >= 48 &&
-	 userKeyObj.getString()->getLength() >= 48 &&
-	 ownerEncObj.isString() &&
-	 ownerEncObj.getString()->getLength() == 32 &&
-	 userEncObj.isString() &&
-	 userEncObj.getString()->getLength() == 32)) {
-      encAlgorithm = cryptRC4;
-      // revision 2 forces a 40-bit key - some buggy PDF generators
-      // set the Length value incorrectly
-      if (encRevision == 2 || !lengthObj.isInt()) {
-	fileKeyLength = 5;
-      } else {
-	fileKeyLength = lengthObj.getInt() / 8;
-      }
-      encryptMetadata = gTrue;
-      //~ this currently only handles a subset of crypt filter functionality
-      //~ (in particular, it ignores the EFF entry in encryptDictA, and
-      //~ doesn't handle the case where StmF, StrF, and EFF are not all the
-      //~ same)
-      if ((encVersion == 4 || encVersion == 5) &&
-	  (encRevision == 4 || encRevision == 5 || encRevision == 6)) {
-	encryptDictA->dictLookup("CF", &cryptFiltersObj);
-	encryptDictA->dictLookup("StmF", &streamFilterObj);
-	encryptDictA->dictLookup("StrF", &stringFilterObj);
-	if (cryptFiltersObj.isDict() &&
-	    streamFilterObj.isName() &&
-	    stringFilterObj.isName() &&
-	    !strcmp(streamFilterObj.getName(), stringFilterObj.getName())) {
-	  if (!strcmp(streamFilterObj.getName(), "Identity")) {
-	    // no encryption on streams or strings
-	    encVersion = encRevision = -1;
-	  } else {
-	    if (cryptFiltersObj.dictLookup(streamFilterObj.getName(),
-					   &cryptFilterObj)->isDict()) {
-	      cryptFilterObj.dictLookup("CFM", &cfmObj);
-	      if (cfmObj.isName("V2")) {
-		encVersion = 2;
-		encRevision = 3;
-		if (cryptFilterObj.dictLookup("Length",
-					      &cfLengthObj)->isInt()) {
-		  //~ according to the spec, this should be cfLengthObj / 8
-		  fileKeyLength = cfLengthObj.getInt();
-		}
-		cfLengthObj.free();
-	      } else if (cfmObj.isName("AESV2")) {
-		encVersion = 2;
-		encRevision = 3;
-		encAlgorithm = cryptAES;
-		if (cryptFilterObj.dictLookup("Length",
-					      &cfLengthObj)->isInt()) {
-		  //~ according to the spec, this should be cfLengthObj / 8
-		  fileKeyLength = cfLengthObj.getInt();
-		}
-		cfLengthObj.free();
-	      } else if (cfmObj.isName("AESV3")) {
-		encVersion = 5;
-		if (encRevision != 5 && encRevision != 6) {
-		  encRevision = 6;
-		}
-		encAlgorithm = cryptAES256;
-		if (cryptFilterObj.dictLookup("Length",
-					      &cfLengthObj)->isInt()) {
-		  //~ according to the spec, this should be cfLengthObj / 8
-		  fileKeyLength = cfLengthObj.getInt();
-		}
-		cfLengthObj.free();
-	      }
-	      cfmObj.free();
-	    }
-	    cryptFilterObj.free();
-	  }
-	}
+  if (!versionObj.isInt() ||
+      !revisionObj.isInt() ||
+      !permObj.isInt() ||
+      !ownerKeyObj.isString() ||
+      !userKeyObj.isString()) {
+    error(errSyntaxError, -1, "Invalid encryption parameters");
+    goto done;
+  }
+  encVersion = versionObj.getInt();
+  encRevision = revisionObj.getInt();
+  encAlgorithm = cryptRC4;
+  // revision 2 forces a 40-bit key - some buggy PDF generators
+  // set the Length value incorrectly
+  if (encRevision == 2 || !lengthObj.isInt()) {
+    fileKeyLength = 5;
+  } else {
+    fileKeyLength = lengthObj.getInt() / 8;
+  }
+  encryptMetadata = gTrue;
+
+  //--- check for a crypt filter (which can modify the parameters)
+  //~ this currently only handles a subset of crypt filter functionality
+  //~ (in particular, it ignores the EFF entry in encryptDictA, and
+  //~ doesn't handle the case where StmF, StrF, and EFF are not all the
+  //~ same)
+  if ((encVersion == 4 || encVersion == 5) &&
+      (encRevision == 4 || encRevision == 5 || encRevision == 6)) {
+    encryptDictA->dictLookup("CF", &cryptFiltersObj);
+    encryptDictA->dictLookup("StmF", &streamFilterObj);
+    encryptDictA->dictLookup("StrF", &stringFilterObj);
+    if (cryptFiltersObj.isDict() &&
+	streamFilterObj.isName() &&
+	stringFilterObj.isName() &&
+	!strcmp(streamFilterObj.getName(), stringFilterObj.getName())) {
+      if (!strcmp(streamFilterObj.getName(), "Identity")) {
+	// no encryption on streams or strings
 	stringFilterObj.free();
 	streamFilterObj.free();
 	cryptFiltersObj.free();
-	if (encryptDictA->dictLookup("EncryptMetadata",
-				     &encryptMetadataObj)->isBool()) {
-	  encryptMetadata = encryptMetadataObj.getBool();
-	}
-	encryptMetadataObj.free();
+	goto done;
       }
-      permFlags = permObj.getInt();
-      ownerKey = ownerKeyObj.getString()->copy();
-      userKey = userKeyObj.getString()->copy();
-      if (encVersion >= 1 && encVersion <= 2 &&
-	  encRevision >= 2 && encRevision <= 3) {
-	if (fileIDObj.isArray()) {
-	  if (fileIDObj.arrayGet(0, &fileIDObj1)->isString()) {
-	    fileID = fileIDObj1.getString()->copy();
-	  } else {
-	    fileID = new GString();
+      if (cryptFiltersObj.dictLookup(streamFilterObj.getName(),
+				     &cryptFilterObj)->isDict()) {
+	cryptFilterObj.dictLookup("CFM", &cfmObj);
+	if (cfmObj.isName("V2")) {
+	  if (cryptFilterObj.dictLookup("Length",
+					&cfLengthObj)->isInt()) {
+	    fileKeyLength = cfLengthObj.getInt();
 	  }
-	  fileIDObj1.free();
-	} else {
-	  fileID = new GString();
+	  cfLengthObj.free();
+	  encVersion = 2;
+	  encRevision = 3;
+	} else if (cfmObj.isName("AESV2")) {
+	  if (cryptFilterObj.dictLookup("Length",
+					&cfLengthObj)->isInt()) {
+	    fileKeyLength = cfLengthObj.getInt();
+	  }
+	  cfLengthObj.free();
+	  encVersion = 2;
+	  encRevision = 3;
+	  encAlgorithm = cryptAES;
+	} else if (cfmObj.isName("AESV3")) {
+	  if (cryptFilterObj.dictLookup("Length",
+					&cfLengthObj)->isInt()) {
+	    fileKeyLength = cfLengthObj.getInt();
+	  }
+	  cfLengthObj.free();
+	  if (fileKeyLength == 16) {
+	    // this isn't allowed by the spec, but Adobe supports it
+	    encVersion = 2;
+	    encRevision = 3;
+	    encAlgorithm = cryptAES;
+	  } else {
+	    encVersion = 5;
+	    if (encRevision != 5 && encRevision != 6) {
+	      encRevision = 6;
+	    }
+	    encAlgorithm = cryptAES256;
+	  }
 	}
-	if (fileKeyLength > 16 || fileKeyLength <= 0) {
-	  fileKeyLength = 16;
-	}
-	ok = gTrue;
-      } else if (encVersion == 5 && (encRevision == 5 || encRevision == 6)) {
-	fileID = new GString(); // unused for V=R=5
-	ownerEnc = ownerEncObj.getString()->copy();
-	userEnc = userEncObj.getString()->copy();
-	if (fileKeyLength > 32 || fileKeyLength <= 0) {
-	  fileKeyLength = 32;
-	}
-	ok = gTrue;
-      } else if (!(encVersion == -1 && encRevision == -1)) {
-	error(errUnimplemented, -1,
-	      "Unsupported version/revision ({0:d}/{1:d}) of Standard security handler",
-	      encVersion, encRevision);
+	cfmObj.free();
       }
-    } else {
-      error(errSyntaxError, -1, "Invalid encryption key length");
+      cryptFilterObj.free();
     }
-  } else {
-    error(errSyntaxError, -1, "Weird encryption info");
+    stringFilterObj.free();
+    streamFilterObj.free();
+    cryptFiltersObj.free();
+    if (encryptDictA->dictLookup("EncryptMetadata",
+				 &encryptMetadataObj)->isBool()) {
+      encryptMetadata = encryptMetadataObj.getBool();
+    }
+    encryptMetadataObj.free();
   }
+
+  //--- version-specific parameters
+  if (encRevision <= 4) {
+    if (ownerKeyObj.getString()->getLength() != 32 ||
+	userKeyObj.getString()->getLength() != 32) {
+      error(errSyntaxError, -1, "Invalid encryption key length");
+      goto done;
+    }
+  } else if (encRevision <= 6) {
+    // the spec says 48 bytes, but Acrobat pads them out longer
+    if (ownerKeyObj.getString()->getLength() < 48 ||
+	userKeyObj.getString()->getLength() < 48 ||
+	!ownerEncObj.isString() ||
+	ownerEncObj.getString()->getLength() != 32 ||
+	!userEncObj.isString() ||
+	userEncObj.getString()->getLength() != 32) {
+      error(errSyntaxError, -1, "Invalid encryption key length");
+      goto done;
+    }
+  }
+  permFlags = permObj.getInt();
+  ownerKey = ownerKeyObj.getString()->copy();
+  userKey = userKeyObj.getString()->copy();
+  if (encVersion >= 1 && encVersion <= 2 &&
+      encRevision >= 2 && encRevision <= 3) {
+    if (fileIDObj.isArray()) {
+      if (fileIDObj.arrayGet(0, &fileIDObj1)->isString()) {
+	fileID = fileIDObj1.getString()->copy();
+      } else {
+	fileID = new GString();
+      }
+      fileIDObj1.free();
+    } else {
+      fileID = new GString();
+    }
+    if (fileKeyLength > 16 || fileKeyLength <= 0) {
+      fileKeyLength = 16;
+    }
+    ok = gTrue;
+  } else if (encVersion == 5 && (encRevision == 5 || encRevision == 6)) {
+    fileID = new GString(); // unused for V=R=5
+    ownerEnc = ownerEncObj.getString()->copy();
+    userEnc = userEncObj.getString()->copy();
+    if (fileKeyLength > 32 || fileKeyLength <= 0) {
+      fileKeyLength = 32;
+    }
+    ok = gTrue;
+  } else {
+    error(errUnimplemented, -1,
+	  "Unsupported version/revision ({0:d}/{1:d}) of Standard security handler",
+	  encVersion, encRevision);
+  }
+
+ done:
   fileIDObj.free();
   permObj.free();
   userEncObj.free();
